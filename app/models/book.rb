b32feef195ab32e8c7555590c728fc8682c802b4
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-02-07 12:22:30 +0000

    log in修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   has_one_attached :image
    belongs_to :user
          
 end
