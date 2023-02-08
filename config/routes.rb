2b83e08e5d2ebb7672053f475a668b6cee45087e
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-02-08 12:22:12 +0000

    headerのrror解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :users
- 
   devise_for :books
   devise_for :users
+  
+  resources :books
+  resources :users
+
   root to: "homes#top"
   get "homes/about" =>"homes#about",as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
