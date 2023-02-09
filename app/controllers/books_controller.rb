a9018ccca3075d607d928b183b9a63371351569f
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-02-09 13:44:37 +0000

    index途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def new
+    @book =Book.new
   end
 
   def creat
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+    @books= Book.all
+    render :index
+    end
   end
 
   def index
+    @user = current_user.name
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -13,4 +25,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+  
 end
