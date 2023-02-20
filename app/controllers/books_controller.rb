6ace63ec9d2e3943fe163eeb3cfcd0ef4d129176
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-02-20 08:18:06 +0000

    0220始まり

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @books = Book.all
+    
   end
 
   def edit
@@ -56,7 +57,8 @@ class BooksController < ApplicationController
     end
     
     def is_matching_login_user
-       user_id = params[:id].to_i
+       book = Book.find(params[:id])
+       user_id = book.user_id
        unless user_id == current_user.id
          redirect_to book_path
        end
